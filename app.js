f2500b3936bc861282cd8db4731b2c2dbd46b904
Mir Nawaz <mir.nawaz@tenpearls.com>
2017-03-27 13:17:10 +0500

    get remote file

--- app.js
+++ app.js
@@ -5,23 +5,20 @@ let express = require('express');
 let lib = require('./lib');
 let path = require('path');
 let config = require('./config.json');
-let program = require('commander');
-
-if (!program.port) program.port = 3000;
 
 let app = express();
 
 global.dir = `${path.resolve(__dirname)}${config.rootDir}`;
-global.host = `http://localhost:${program.port}/`;
+global.host = `http://localhost:${config.port}/`;
 global.home = __dirname;
 global.webDir = `${__dirname}/web`;
 
 app.use(express.static(global.dir));
 app.use(express.static(global.webDir));
 
-http.createServer(app).listen(program.port);
+http.createServer(app).listen(config.port);
 
-console.log(`Please open the link in your browser http://<YOUR-IP>: ${program.port}`);
+console.log(`Please open the link in your browser http://<YOUR-IP>: ${config.port}`);
 
 app.get('/files', lib.get);
 
